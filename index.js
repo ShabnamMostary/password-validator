7818c392c284d3c9cbd93c8ba7a99c7ba61d91c3
shabnam <shabnam.mostary@gmail.com>
2020-10-16 16:39:39 -0400

    added code for special character

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 function validatePass(password) {
   var l = password.length
+  var spChars = '~`!#$%^&*+=-[]\\\';,/{}|":<>?'
   var lengthCheck = false
   var numberCheck = false
+  var specialCharCheck = false
+  var upperCaseCheck = false
+  var lowerCaseCheck = false
 
 
   if (l >= 8) {
     lengthCheck = true
   }
-  for (var i = 0; i < l; i++) {
+  for (let i = 0; i < l; i++) {
     if (password[i] >= '0' && password[i] <= '9') {
       numberCheck = true
     }
   }
 
-  return (lengthCheck && numberCheck)
+
+  for (let i = 0; i < l; i++) {
+    if (spChars.indexOf(password[i]) >= 0) {
+      specialCharCheck = true
+    }
+  }
+
+
+
+  return (lengthCheck && numberCheck && upperCaseCheck && lowerCaseCheck && specialCharCheck)
 }
 
 module.exports = validatePass
